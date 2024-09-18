60dcd2f5d164b2e6dbd3491a156c23545c1a922f
Md.Abdur Rahman <ar.minhazul@gmail.com>
2024-09-18 06:44:20 +0600

    added forEach function

--- script.js
+++ script.js
@@ -4,19 +4,34 @@ const todoList = [{name:'Physics Homework',
 renderTodoList();
 function renderTodoList(){
     let todoListHTML = '';
-    for (let i = 0; i<todoList.length;i++){
-        const todoObject = todoList[i];
-        // const name = todoObject.name;
-        //const dueDate = todoObject.dueDate;
+    todoList.forEach(function(todoObject,index){
         const {name,dueDate} = todoObject;
         const html = 
           `<div>${name}</div>
           <div>${dueDate} </div> 
           <button onclick="
-            todoList.splice(${i},1); renderTodoList()
-          " class="delete-todo-button">Delete</button>`
+            todoList.splice(${index},1); renderTodoList()
+          " class="delete-todo-button">Delete</button>`;
         todoListHTML += html;
-    }
+    
+
+    });
+
+    //added forEach so i comment out them
+    // for (let i = 0; i<todoList.length;i++){
+    //     const todoObject = todoList[i];
+    //     // const name = todoObject.name;
+    //     //const dueDate = todoObject.dueDate;
+    //     const {name,dueDate} = todoObject;
+    //     const html = 
+    //       `<div>${name}</div>
+    //       <div>${dueDate} </div> 
+    //       <button onclick="
+    //         todoList.splice(${i},1); renderTodoList()
+    //       " class="delete-todo-button">Delete</button>`
+    //     todoListHTML += html;
+    // }
+        
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
 
